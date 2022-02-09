bfcf1ef6b699f9b5b7f1bac13f274b3e8647e5aa
Michael Terrell <cwithmichael@gmail.com>
2022-02-08 23:58:30 -0600

    improve naming

--- react.go
+++ react.go
@@ -5,16 +5,16 @@ import (
 	"time"
 )
 
-type MyReactor struct {
-	computeCells []MyComputeCell
+type CellReactor struct {
+	computeCells []ReactiveComputeCell
 }
 
-func (r *MyReactor) CreateInput(value int) InputCell {
-	return &MyInputCell{value: value, notifyWorld: func() { r.NotifyComputeCells() }}
+func (r *CellReactor) CreateInput(value int) InputCell {
+	return &StimulusInputCell{value: value, notifyWorld: func() { r.NotifyComputeCells() }}
 }
 
-func (r *MyReactor) CreateCompute1(cell Cell, cb func(int) int) ComputeCell {
-	r.computeCells = append(r.computeCells, MyComputeCell{
+func (r *CellReactor) CreateCompute1(cell Cell, cb func(int) int) ComputeCell {
+	r.computeCells = append(r.computeCells, ReactiveComputeCell{
 		cells:           []Cell{cell},
 		computeFunction: func() int { return cb(cell.Value()) },
 		prevComputed:    cb(cell.Value()),
@@ -22,8 +22,8 @@ func (r *MyReactor) CreateCompute1(cell Cell, cb func(int) int) ComputeCell {
 	return &r.computeCells[len(r.computeCells)-1]
 }
 
-func (r *MyReactor) CreateCompute2(cell Cell, cell2 Cell, cb func(int, int) int) ComputeCell {
-	r.computeCells = append(r.computeCells, MyComputeCell{
+func (r *CellReactor) CreateCompute2(cell Cell, cell2 Cell, cb func(int, int) int) ComputeCell {
+	r.computeCells = append(r.computeCells, ReactiveComputeCell{
 		cells:           []Cell{cell, cell2},
 		computeFunction: func() int { return cb(cell.Value(), cell2.Value()) },
 		prevComputed:    cb(cell.Value(), cell2.Value()),
@@ -31,7 +31,7 @@ func (r *MyReactor) CreateCompute2(cell Cell, cell2 Cell, cb func(int, int) int)
 	return &r.computeCells[len(r.computeCells)-1]
 }
 
-func (r *MyReactor) NotifyComputeCells() {
+func (r *CellReactor) NotifyComputeCells() {
 	for _, cc := range r.computeCells {
 		// If the computed value has changed, then this will trigger the callbacks
 		cc.Value()
@@ -40,19 +40,19 @@ func (r *MyReactor) NotifyComputeCells() {
 
 func New() Reactor {
 	rand.Seed(time.Now().UnixNano())
-	return &MyReactor{}
+	return &CellReactor{}
 }
 
-type MyInputCell struct {
+type StimulusInputCell struct {
 	value       int
 	notifyWorld func()
 }
 
-func (ic *MyInputCell) Value() int {
+func (ic *StimulusInputCell) Value() int {
 	return ic.value
 }
 
-func (ic *MyInputCell) SetValue(value int) {
+func (ic *StimulusInputCell) SetValue(value int) {
 	prev := ic.value
 	ic.value = value
 	// Have to do it this way because once we notify the world
@@ -62,24 +62,24 @@ func (ic *MyInputCell) SetValue(value int) {
 	}
 }
 
-type MyCancel struct {
+type Cancel struct {
 	cbId int
-	cc   *MyComputeCell
+	cc   *ReactiveComputeCell
 }
 
-func (c *MyCancel) Cancel() {
+func (c *Cancel) Cancel() {
 	delete(c.cc.cbs, c.cbId)
 }
 
-type MyComputeCell struct {
+type ReactiveComputeCell struct {
 	prevComputed    int
 	cbs             map[int]func(int)
 	cells           []Cell
 	computeFunction func() int
-	MyCancel        MyCancel
+	Cancel          Canceler
 }
 
-func (cc *MyComputeCell) Value() int {
+func (cc *ReactiveComputeCell) Value() int {
 	computedValue := cc.computeFunction()
 	// Only call the callbacks if the computed value has changed
 	if cc.prevComputed != computedValue {
@@ -91,11 +91,11 @@ func (cc *MyComputeCell) Value() int {
 	return computedValue
 }
 
-func (cc *MyComputeCell) AddCallback(cb func(value int)) Canceler {
+func (cc *ReactiveComputeCell) AddCallback(cb func(value int)) Canceler {
 	if cc.cbs == nil {
 		cc.cbs = make(map[int]func(int))
 	}
 	cbId := rand.Int()
 	cc.cbs[cbId] = cb
-	return &MyCancel{cbId: cbId, cc: cc}
+	return &Cancel{cbId: cbId, cc: cc}
 }
